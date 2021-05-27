d8082de8b6202d89c0b5d2d2336761513e0e72f5
Claudio Ortega <claudio.rtega2701@gmail.com>
2021-05-27 02:09:47 -0500

    batch size needs to match model input size

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import os
 from transformers import AutoTokenizer
 
-MAX_LEN = 1024 # Might have to set to a higher value (use dataset to calculate)
+MAX_LEN = 512 # Might have to set to a higher value (use dataset to calculate)
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 16
 EPOCHS = 10
