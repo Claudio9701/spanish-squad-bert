3531b3ff47ba826ce33a5f6d9c29eeefdee32d3b
Claudio Ortega <claudio.rtega2701@gmail.com>
2021-05-26 23:54:16 -0500

    update max_len according to dataset & use UdeChile spanish bert

--- src/config.py
+++ src/config.py
@@ -1,14 +1,14 @@
 import os
-import tokenizers
+from transformers import AutoTokenizer
 
-MAX_LEN = 32
+MAX_LEN = 1024 # Might have to set to a higher value (use dataset to calculate)
 TRAIN_BATCH_SIZE = 32
 VALID_BATCH_SIZE = 16
 EPOCHS = 10
-BERT_PATH = '../input/bert_base_uncased/'
+BERT_PATH = 'dccuchile/bert-base-spanish-wwm-cased'
 MODEL_PATH = 'model.bin'
 TRAINING_FILE = '../input/train.csv'
-TOKENIZER = tokenizers.BertWordPieceTokenizer(
-    os.path.join(BERT_PATH, 'vocab.txt'),
+TOKENIZER = AutoTokenizer.from_pretrained(
+    BERT_PATH,
     lowercase=True
 )
